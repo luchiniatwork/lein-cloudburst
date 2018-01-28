288427d5378f07723feb2cecc5bf457c89e64f17
Tiago Luchini <luchini@work.co>
2018-01-27 21:02:13 -0500

    Add clojure 1.9 and clojurescript deps

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[environ "1.1.0"]
+                 [org.clojure/clojure "1.9.0"]
                  [org.clojure/core.async "0.4.474"]
+                 [org.clojure/clojurescript "1.9.946"]
                  [org.clojure/data.codec "0.1.1"]
                  [org.sharetribe/aws-sig4 "0.1.3"]
                  [cheshire "5.8.0"]
